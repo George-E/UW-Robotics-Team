f254774924111be84a5202786f8425b2fcb11b07
George Eisa <george.eisa7@gmail.com>
2016-10-23 15:06:54 -0400

    Drive Code
    Added line reading logic and motor controls

--- T800_Program/T800_Program.ino
+++ T800_Program/T800_Program.ino
@@ -10,6 +10,16 @@ int timerCount = 0;
 bool countingUp = true;
 int test = 0;
 int note = 0;
+
+#define RSpeedPin  11
+#define RDirPin  8
+#define LSpeedPin  10
+#define LDirPin  12
+#define lineGrayscaleSensor  0
+#define musicGrayscaleSensor  2
+int Speed = 10;
+int edge = 500;
+
 void setup() {
   Serial.begin(9600);
   noteTimer.begin(playNote,0.5);
@@ -17,8 +27,7 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  followLine();
 }
 
 void drive (int s, int dir) {
@@ -44,7 +53,17 @@ void colourToNote () {
 }
 
 void followLine() {
-
+  //assumed black is higher, have to check that
+  //values will have to be try tested of course
+  int lineReading = readGrayscaleSensor(lineGrayscaleSensor);
+  int difference = lineReading- edge;
+  int leftMultiplier =0, rightMultiplier = 0;
+  if (difference > 0) {
+    leftMultiplier -= abs(difference)/100.0;
+  } else {
+    rightMultiplier -= abs(difference)/100.0;
+  }
+  forward(leftMultiplier,rightMultiplier);
 }
 
 void convertToBin(int num, int a[]) {
@@ -68,4 +87,33 @@ void count(){
   Serial.print(timerCount);
   Serial.print("\n");
 }
+
+int readGrayscaleSensor(int pin) {
+  return analogRead(pin);
+}
+
+void stop() {
+  digitalWrite( RSpeedPin, LOW );
+  digitalWrite( RDirPin, LOW );
+  digitalWrite( LSpeedPin, LOW );
+  digitalWrite( LDirPin, LOW );
+}
+
+
+void forward(double leftMultiplier, double rightMultiplier)
+{
+  digitalWrite(RDirPin, HIGH);
+  analogWrite(RSpeedPin, Speed*rightMultiplier);
+  digitalWrite(LDirPin, HIGH);
+  analogWrite(LSpeedPin, Speed*leftMultiplier);
+}
+
+void backward(double leftMultiplier, double rightMultiplier)
+{
+  digitalWrite(RDirPin, LOW);
+  analogWrite(RSpeedPin, Speed*rightMultiplier);
+  digitalWrite(LDirPin, LOW);
+  analogWrite(LSpeedPin, Speed*leftMultiplier);
+}
+
 
