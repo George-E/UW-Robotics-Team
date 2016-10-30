2017fc2db6c8271a4a84cf9f393a28396497661a
George Eisa <george.eisa7@gmail.com>
2016-10-29 23:51:39 -0400

    Many Fixes to Sound Card Bugs
    Changed Timer to use TimerOne Teensy library
    Precomputed sin values to make loop run faster

--- T800_Program/T800_Program.ino
+++ T800_Program/T800_Program.ino
@@ -1,9 +1,5 @@
-void drive (int s, int dir);
-void playNote ();
-void colourToNote();
-void followLine();
-void convertToBin(int num, int a[]);
 
+#include <TimerOne.h>
 double fqcy[] = {0,261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25};
 //<<<<<<< HEAD
 //float timePerStep[] = {0,7.5f,6.7f, 6.0f, 5.6f, 5.0f, 4.5f,4.0f,3.7f};//values for the timer (in microseconds)
@@ -12,10 +8,9 @@ int noteColourRanges[] = {170,195,225,265,305,345,900,900,900};
 //=======
 
 //>>>>>>> origin/master
-int noteTimerCount = 0;
-IntervalTimer noteTimer;
 
 
+int sinVals[250] = {0};
 int test = 0;
 int note = 3;
 
@@ -24,40 +19,71 @@ int note = 3;
 #define LSpeedPin  10
 #define LDirPin  12
 #define lineGrayscaleSensor  A0
-#define musicGrayscaleSensor  A2
+#define musicGrayscaleSensor  A1
 int Speed = 10;
 int edge = 500;
 
+int noteTimerCount = 0;
+//IntervalTimer noteTimer;
 
 void setup() {
-  
+  for (int i=0; i<250;i++) {
+    sinVals[i] = (int) (127.5+127.5*sin(3.141592654*2*(double)i/250) +0.5);//+.5 so rounding occurs
+    }
   Serial.begin(9600);
-  noteTimer.begin(playNote,0.1f);
 
+    pinMode(13,OUTPUT);
+  digitalWrite(13, LOW);//so we know its on
+  
   for(int i = 0; i < 8; i++) pinMode(i,OUTPUT);
   
-
+  //noteTimer.begin(playNote,0.1f);
+  Timer1.initialize(40);
+  Timer1.attachInterrupt(playNote);
+ Timer1.start();
+  
 }
+
 
 void loop() {
   //followLine();
+  /*int bin[8] = {0};
+  for (int n=0; n<256;n++) {
+    convertToBin(n,bin);
+    for(int i = 0; i < 8; i++){
+     // Serial.print(bin[i]);
+      if(bin[8-1-i] == 1) digitalWrite(i,HIGH);//added -1 b/c we want 0-7 not 1-8
+      else digitalWrite(i,LOW); 
+    }
+    delayMicroseconds(10);
+  }
+  for (int n=255; n>=0;n--) {
+    convertToBin(n,bin);
+    for(int i = 0; i < 8; i++){
+     // Serial.print(bin[i]);
+      if(bin[8-1-i] == 1) digitalWrite(i,HIGH);//added -1 b/c we want 0-7 not 1-8
+      else digitalWrite(i,LOW); 
+    }
+    delayMicroseconds(10);
+  }*/
 }
 
-void drive (int s, int dir) {
-  //DRIVING CODE HERE
-}
 
 // Plays a note in a set octave
 void playNote (void) {//where is note var used, and does it know not to play anything if note == 0?
-  noteTimerCount++;
-
+  
   int bin[8] = {0};
   if(note != 0){
-    convertToBin((int)(127.5+127.5*sin(2*(3.141592654)*fqcy[note]*0.0000001*noteTimerCount) +0.5),bin); //added the plus 0.5 so that the cast to int will essentially follow traditional rounding standards 
+    //int num = (int)(127.5+127.5*sin(2*(3.141592654)*fqcy[note]*0.0000001*noteTimerCount) +0.5);
+    int num = sinVals[noteTimerCount];
+    Serial.println(sinVals[noteTimerCount]);
+    convertToBin(num,bin); //added the plus 0.5 so that the cast to int will essentially follow traditional rounding standards 
     for(int i = 0; i < 8; i++){
-      if(bin[8-i] == 1) digitalWrite(i,HIGH);
+     // Serial.print(bin[i]);
+      if(bin[7-i] == 1) digitalWrite(i,HIGH);//changed 8 to 7 b/c we want 0-7 not 1-8
       else digitalWrite(i,LOW); 
     }
+    //Serial.println();
   }
 
   else {
@@ -65,9 +91,9 @@ void playNote (void) {//where is note var used, and does it know not to play any
       digitalWrite(i,LOW);
     }
   }
-
+noteTimerCount++;
                         //the timer will tick every time per level, and every tick will increase the value of a counter variable
-if (noteTimerCount == 10000000) noteTimerCount = 0; //after one second, restarts
+if (noteTimerCount == 250) noteTimerCount = 0; //after one second, restarts
 }
 
 void colourToNote () {
